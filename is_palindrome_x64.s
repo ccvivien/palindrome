4bd5041d5dc093ff608bf9888229bf881a4f7684
Vivien B <vivien@captaincontrat.com>
2022-02-25 17:43:51 +0100

    Indent

--- is_palindrome_x64.s
+++ is_palindrome_x64.s
@@ -3,30 +3,30 @@
 .section .text
 
 .type is_palindrome, @function
-/* rdi char* s */
-/* rsi long len */
+/* rdi -> char* s */
+/* rsi -> long len */
 is_palindrome:
-   mov    $0, %eax      /* i */
+	xor	%rax, %rax    /* i = 0 */
 
-   mov    %rsi, %rcx
-   shr    %rcx          /* len / 2 */
+	mov	%rsi, %rcx
+	shr	%rcx          /* len / 2 */
 
-   mov    %rsi, %rdx
-   dec    %rdx          /* j = len - 1 */
+	mov	%rsi, %rdx
+	dec	%rdx          /* j = len - 1 */
 
 loop:
-  movzbq (%rdi, %rdx), %r8
-  cmp    %r8b, (%rdi, %rax)
-  jne    not_palindrome
+	movzbq	(%rdi, %rdx), %r8
+	cmp	%r8b, (%rdi, %rax)
+	jne	not_palindrome
 
-  inc    %rax
-  dec    %rdx
-  cmp    %rcx, %rax
-  jl     loop
+	inc	%rax
+	dec	%rdx
+	cmp	%rcx, %rax
+	jl	loop
 
-  mov    $1, %eax
-  retq
+	mov	$1, %eax
+	retq
 
 not_palindrome:
-  mov    $0, %eax
-  retq
+	xor	%rax, %rax
+	retq
